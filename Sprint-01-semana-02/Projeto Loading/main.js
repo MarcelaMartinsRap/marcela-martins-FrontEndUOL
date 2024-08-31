139c51ca9b89cdd6c3ea156728a886623bf714f5
Marcela <marcelamartinstec@gmail.com>
2024-08-31 10:47:30 -0300

    Alteração nome de pasta

---
+++ Sprint-01-semana-02/Projeto Loading/main.js
@@ -0,0 +1,4 @@
+//Essa função cria um tempo pré determinado de 4 segundos para redirecinamento mostrado no 4000 como milisegundos.
+setTimeout(function() {
+    window.location.href = "intro.html";
+}, 4000);
